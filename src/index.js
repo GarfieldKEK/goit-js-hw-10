6b6a861f1067ca3e6b0260eef2ed4355bc5f0b23
GarfieldKEK <g0ogles@ukr.net>
2023-05-10 16:41:33 +0300

    PLS

--- src/index.js
+++ src/index.js
@@ -1,4 +1,84 @@
 import './css/styles.css';
-
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
+import {fetchCountries} from "./fetchCountries.js"
 const DEBOUNCE_DELAY = 300;
-console.log(4);
+
+// function fetchCountries(name) {
+//   return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
+//     .then(response => {
+//       if (!response.ok) {
+//         throw new Error(`HTTP error! status: ${response.status}`);
+//       }
+//       return response.json();
+//     })
+//     .then(data => {
+//       return data;
+//     })
+//     .catch(error => {
+//       console.error(error);
+//       throw error;
+//     });
+// }
+
+function renderCountriesList(countries) {
+  const list = document.querySelector('.country-list');
+  list.innerHTML = '';
+  countries.forEach(country => {
+    const item = document.createElement('li');
+    console.log(country);
+    const flag = document.createElement('img');
+    flag.src = country.flags.svg;
+    flag.alt = country.name.official;
+    const name = document.createElement('span');
+    name.textContent = country.name.official;
+    item.append(flag, name);
+    list.append(item);
+  });
+}
+
+function renderCountryInfo(country) {
+  const info = document.querySelector('.country-info');
+  info.innerHTML = '';
+  const flag = document.createElement('img');
+  flag.src = country.flags.svg;
+  flag.alt = country.name.official;
+  const name = document.createElement('h2');
+  name.textContent = country.name.official;
+  const capital = document.createElement('p');
+  capital.textContent = `Capital: ${country.capital}`;
+  const population = document.createElement('p');
+  population.textContent = `Population: ${country.population}`;
+  const languages = document.createElement('ul');
+  country.languages.forEach(lang => {
+    const item = document.createElement('li');
+    item.textContent = lang.name;
+    languages.append(item);
+  });
+  info.append(flag, name, capital, population, languages);
+}
+
+function showTooManyMatchesAlert() {
+  Notiflix.Notify.warning('Too many matches found. Please enter a more specific name.');
+}
+
+const input = document.querySelector('#search-box');
+input.addEventListener('input', debounce(() => {
+  const query = input.value.trim();
+  if (!query) {
+    return;
+  }
+    fetchCountries(query)
+    .then(countries => {
+      if (countries.length > 10) {
+        showTooManyMatchesAlert();
+        return;
+      }
+      if (countries.length > 1) {
+        renderCountriesList(countries);
+        return;
+      }
+      renderCountryInfo(countries[0]);
+    })
+    .catch(error => Notiflix.Notify.failure("Ops, there is no country with that name"))
+}, DEBOUNCE_DELAY));
